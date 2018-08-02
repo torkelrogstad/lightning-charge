3673576e3c7f135338d43324e5b00ca31acd2106
Chris Stewart <stewart.chris1234@gmail.com>
2018-08-02 15:41:19 -0500

    updating api to query joe flacco and mitchell trubisky stats
    Turn down logging
    
    Updating setInterval time to 30 seconds

--- src/websocket.js
+++ src/websocket.js
@@ -33,7 +33,7 @@ module.exports = (app, payListen, ln) => {
       createWebSocket: url => new W3CWebSocket(url),
       packMessage: data => {
         const stringify = JSON.stringify(data);
-         console.log("API Request:  " + stringify + "\n");
+         //console.log("API Request:  " + stringify + "\n");
          return stringify;
        },
       unpackMessage: message => {
@@ -52,17 +52,27 @@ module.exports = (app, payListen, ln) => {
 
     
     wsp.open()
-    .then(() => infoEvery5sec(wsp))
+    .then(() => loop(wsp))
 
-    function infoEvery5sec(wsp) {
+    function loop(wsp) {
+      //first one request initially, and then wait 30 seconds to query our api repeatedly
+      joe_flacco_stats_this_week()
+        .then(msg => console.log("\nJoe Flacco stats: " + JSON.stringify(msg) + "\n"));
+	
+      mitchell_trubisky_stats_this_week()
+        .then(msg => console.log("\nMitchell Trubisky stats: " + JSON.stringify(msg) + "\n"));
       setInterval(function () {
 	//Uncomment next two lines if you want to look at tom brady's super bowl stats
-        //tom_brady_superbowl_stats()
-        //  .then(msg => console.log("\nAPI response: " + JSON.stringify(msg) + "\n"));
+        
+	joe_flacco_stats_this_week()
+          .then(msg => console.log("\nJoe Flacco stats: " + JSON.stringify(msg) + "\n"));
+	
+	mitchell_trubisky_stats_this_week()
+	  .then(msg => console.log("\nMitchell Trubisky stats: " + JSON.stringify(msg) + "\n"));
 
-	sb_client.info(wsp).then(msg => console.log("\nAPI response: " + JSON.stringify(msg) + "\n"));
+	//sb_client.info(wsp).then(msg => console.log("\nAPI response: " + JSON.stringify(msg) + "\n"));
 
-	},10000)
+	},30000)
     };
 
     function tom_brady_superbowl_stats() {
@@ -77,8 +87,51 @@ module.exports = (app, payListen, ln) => {
 	    "passing",
 	    g[0]['gsisId'],
 	    p[0]['playerId'])));
+
       return result;
     }
+
+    function joe_flacco_stats_this_week() { 
+      return player_stats_this_week("Flacco", "Joe", "passing");
+    }
+
+    function mitchell_trubisky_stats_this_week() {
+      return player_stats_this_week("Trubisky", "Mitchell", "passing");
+    }
+  
+    /** Returns the player's stats for the given week */
+    function player_stats_this_week(lastname, firstname, stattype) { 
+      //{"version":"8","lastRosterDownload":"20180801T153829.816Z","seasonType":"Regular","seasonYear":2017,"week":"NflWeek17"}
+      const playerP = sb_client.player(wsp, lastname, firstname);
+      const metaP = sb_client.info(wsp);
+      const gameP =  metaP.then(m => sb_client.games(
+        wsp,
+        parseWeek(m['week']),
+	m['seasonType'], 
+	m['seasonYear'], 
+	'CHI'
+      ));
+
+      const stats = playerP.then(p => 
+        gameP.then(g =>
+          sb_client.stats_game_player_id(
+            wsp, 
+	    stattype,
+	    g[0]['gsisId'],
+	    p[0]['playerId']
+          )
+        )
+      );
+
+      return stats;
+    }
+
+    function parseWeek(week) {
+      const weekString = week.split('k')[1];
+      return parseInt(weekString);
+    }
   }
+
+
   return { start };
 }
